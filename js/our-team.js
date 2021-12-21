96d2a9a89d2ece00a759fe7f802bec2abff0fe08
AbdoArafh <abdoarafh54@gmail.com>
2021-12-21 09:27:30 +0200

    done with homepage

--- js/our-team.js
+++ js/our-team.js
@@ -1,5 +1,5 @@
 window.addEventListener("DOMContentLoaded", function() {
-    var glider = new Glider(document.querySelector(".glider"), {
+    var glider = new Glider(document.querySelector(".our-team .glider-contain .glider"), {
         slidesToShow: 'auto',
         slidesToScroll: 1,
         itemWidth: 300,
